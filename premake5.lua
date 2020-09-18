aaf5be9b23cd1aea6858044f0c53d05240ccdef2
TheOrestes <rupeshmandke@outlook.com>
2020-09-18 20:00:10 +0530

    Added PreMake based Project generation system

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "GameEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine"
+	location "GameEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/Src/**.h",
+		"%{prj.name}/Src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/ThirdParty"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PLATFORM_WINDOWS",
+			"ENGINE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Game")
+		}
+
+	filter "configurations:Debug"
+		defines "_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "_NDEBUG"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "_NDEBUG"
+		optimize "On"
+
+project "Game"
+	location "Game"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/Src/**.h",
+		"%{prj.name}/Src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GameEngine/ThirdParty",
+		"GameEngine/Src"
+	}
+
+	links
+	{
+		"GameEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "_NDEBUG"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "_NDEBUG"
+		optimize "On"
